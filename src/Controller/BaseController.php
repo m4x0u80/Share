3c77d07b0b197c3f2c15619defe0de29b543f985
m4x0u80 <max80ad@gmail.com>
2024-10-01 08:52:56 +0200

    initialisaion

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Contact;
+use App\Entity\Categorie;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\CategorieType;
+
+class BaseController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('base/index.html.twig', [
+        ]);
+    }
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(Request $request, EntityManagerInterface $em): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($contact);
+                $em->flush();
+                $this->addFlash('notice','Message envoyé');
+                return $this->redirectToRoute('app_contact');
+
+            }
+        }
+        return $this->render('base/contact.html.twig', [
+            'form' => $form->createView(),
+
+        ]);
+    }
+
+    #[Route('/categorie', name: 'app_categorie')]
+    public function categorie(Request $request) : Response
+    {
+        $categorie = new Categorie();
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $this->addFlash('notice','Message envoyé');
+                return $this->redirectToRoute('app_categorie');
+            }
+        }
+           
+
+        return $this->render('base/categorie.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/apropos', name: 'app_apropos')]
+    public function apropos(): Response
+    {
+        return $this->render('base/apropos.html.twig', [
+
+        ]);
+    }
+    #[Route('/mentions légales', name: 'app_mentionslegales')]
+    public function mentionslegales(): Response
+    {
+        return $this->render('base/mentionslegales.html.twig', [
+
+        ]);
+    }
+
+}
